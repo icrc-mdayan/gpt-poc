8ae78eee710221afc013195fcc64ed4b792d317d
cs210-bbernath~ <bastien.bernath@epfl.ch>
2024-07-08 15:27:03 +0200

    requirements changed

--- retriever/embedding.py
+++ retriever/embedding.py
@@ -1,40 +1,8 @@
 import os
 import json
-
-# from torch import Tensor
-# from transformers import AutoTokenizer, AutoModel
-
-# def get_detailed_instruct(task_description: str, query: str) -> str:
-#     return f'Instruct: {task_description}\nQuery: {query}'
-
-# def last_token_pool(last_hidden_states: Tensor,
-#                  attention_mask: Tensor) -> Tensor:
-#     left_padding = (attention_mask[:, -1].sum() == attention_mask.shape[0])
-#     if left_padding:
-#         return last_hidden_states[:, -1]
-#     else:
-#         sequence_lengths = attention_mask.sum(dim=1) - 1
-#         batch_size = last_hidden_states.shape[0]
-#         return last_hidden_states[torch.arange(batch_size, device=last_hidden_states.device), sequence_lengths]
-
-# model = SentenceTransformer("Alibaba-NLP/gte-Qwen2-1.5B-instruct", trust_remote_code=True)
-# # In case you want to reduce the maximum length:
-# model.max_seq_length = 8192
-# #read all the documents in icrc_split.jsonl
-# for root, dirs, files in os.walk('icrc_split.jsonl'):
-#     for filename in files:
-#         if filename.endswith('.json'):
-#             file_path = os.path.join(root, filename)
-#             with open(file_path, 'r') as json_file:
-#                 data = json.load(json_file)
-#             #get the embeddings for each sequence
-#             embeddings = model.encode(data)
-#             #save the embeddings to a new file
-#             name = filename.split('.')[0] + f'_embeddings.json'
-#             new_file_path = os.path.join('icrc_embeddings.jsonl', name)
-#             with open(new_file_path, 'w') as new_json_file:
-#                 json.dump(embeddings, new_json_file)
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import voyageai
 import numpy as np
 
 def k_nearest_neighbors(query_embedding, documents_embeddings, k=5):
@@ -57,29 +25,11 @@ def k_nearest_neighbors(query_embedding, documents_embeddings, k=5):
 
   return top_k_related_embeddings, top_k_related_indices
 
-import voyageai
+
 voyageai.api_key = "pa-dQ28MnYTDY5xF72HnbBVB5-w9FCs7E6yzzZBbAn0YPk"
     
 vo = voyageai.Client()
-import numpy as np
-# data = []
-# for root, dirs, files in os.walk('icrc_split.jsonl'):
-#     for filename in files:
-#         if filename.endswith('.json'):
-#             file_path = os.path.join(root, filename)
-#             with open(file_path, 'r') as json_file:
-#                 data.append(json.load(json_file))
-# print(len(data))
-# batch_size = 128
-# embeddings = []
-# for i in range(0, len(data), batch_size):
-#     embedding = vo.embed(
-#         data[i:i + batch_size], model="voyage-large-2-instruct", input_type="document"
-#     ).embeddings
-#     embeddings.append(embedding)
 
-# with open('icrc_embeddings.jsonl', 'w') as json_file:
-#     json.dump(embeddings, json_file)
 with open('icrc_embeddings.jsonl', 'r') as json_file:
     documents_embeddings = json.load(json_file)
 print(len(documents_embeddings))

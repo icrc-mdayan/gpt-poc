8ef809918a13e5807cbbf2955c2980972a21bb9b
cs210-bbernath~ <bastien.bernath@epfl.ch>
2024-07-12 09:53:46 +0200

    rag mode

--- rag_mode.py
+++ rag_mode.py
@@ -8,16 +8,16 @@ def run_rag_mode():
     st.title('RAG-mode')
 
     # Store LLM generated responses
-    if "messages" not in st.session_state.keys():
-        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    if "rag_messages" not in st.session_state.keys():
+        st.session_state.rag_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
     # Display or clear chat messages
-    for message in st.session_state.messages:
+    for message in st.session_state.rag_messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
     def clear_chat_history():
-        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+        st.session_state.rag_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
     def llm_generate_response(prompt_input):
@@ -31,24 +31,29 @@ def run_rag_mode():
 
     def generate_response(prompt_input):
         system_prompt = "You are a helpful medical assistant."
-        if st.session_state.messages is None:
-            prompt = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_input}]
-        else:
-            prompt = st.session_state.messages
         #prompt.append({"role": "user", "content": prompt_input})
         documents = retrieve_documents(prompt_input)
-        print(documents)
-        output = llm_generate_response(prompt)
+        with st.sidebar:
+            st.write("## Retrieved Documents")
+            for idx, doc in enumerate(documents, start=1):
+                with st.expander(f"Document {idx}"):
+                    st.write(doc)  
+
+        # Now generate the response using the documents (if necessary) and prompt
+        question = [{"role": "system", "content": system_prompt}] + st.session_state.rag_messages
+        question[-1]["content"] = "<Document>:  ".join(documents) + " Question:  " + question[-1]["content"]
+        output = llm_generate_response(question)
+
         return output
 
     # User-provided prompt
     if prompt := st.chat_input("What bothers you today?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.session_state.rag_messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.write(prompt)
 
     # Generate a new response if last message is not from assistant
-    if st.session_state.messages[-1]["role"] != "assistant":
+    if st.session_state.rag_messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 response = generate_response(prompt)
@@ -59,4 +64,4 @@ def run_rag_mode():
                     placeholder.markdown(full_response)
                 placeholder.markdown(full_response)
         message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
+        st.session_state.rag_messages.append(message)

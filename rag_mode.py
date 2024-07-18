872d6f58d8a325acccfc6ffa608098a97d852255
cs210-bbernath~ <bastien.bernath@epfl.ch>
2024-07-18 16:26:09 +0200

    few tweaks here and there

--- rag_mode.py
+++ rag_mode.py
@@ -21,7 +21,7 @@ def run_rag_mode():
 
     def llm_generate_response(prompt_input):
         client = OpenAI(base_url="http://104.171.203.227:8000/v1", api_key="EMPTY")
-        response = client.chat.completions.create(model="llama-3-70b-meditron", messages=prompt_input)
+        response = client.chat.completions.create(model="llama-2-70b-meditron", messages=prompt_input)
         return response.choices[0].message.content
 
     def generate_response(prompt_input):
@@ -29,19 +29,21 @@ def run_rag_mode():
             system_prompt = file.read().strip()
         #prompt.append({"role": "user", "content": prompt_input})
         documents = retrieve_documents(prompt_input)
-        with st.sidebar:
-            st.write("## Retrieved Documents")
-            for idx, doc in enumerate(documents, start=1):
-                with st.expander(f"Document {idx}"):
-                    st.write(doc)  
+        if documents != []:
+            with st.sidebar:
+                st.write("## Retrieved Documents")
+                for idx, doc in enumerate(documents, start=1):
+                    with st.expander(f"Document {idx}"):
+                        st.write(doc)  
 
-        # Now generate the response using the documents (if necessary) and prompt
-        question = [{"role": "system", "content": system_prompt}]
-        question.extend(st.session_state.rag_messages[:-1])
-        formatted_documents = "\n".join([f"Document {idx+1}:\n{doc}" for idx, doc in enumerate(documents)])
-        question.append({"role": "user", "content": f"{formatted_documents}\n\nQuestion: {prompt_input}"})
-        print(question)
-        output = llm_generate_response(question)
+            # Now generate the response using the documents (if necessary) and prompt
+            question = [{"role": "system", "content": system_prompt}]
+            question.extend(st.session_state.rag_messages[:-1])
+            formatted_documents = "\n".join([f"Document {idx+1}:\n{doc}" for idx, doc in enumerate(documents)])
+            question.append({"role": "user", "content": f"{formatted_documents}\n\nQuestion: {prompt_input}"})
+            output = llm_generate_response(question)
+        else:
+            output = llm_generate_response(st.session_state.rag_messages)
 
         return output
 

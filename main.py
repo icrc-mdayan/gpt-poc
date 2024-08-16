e148a27faee83f58049650422307cc099c0d2863
Etienne <etien@pop-os.localdomain>
2024-08-16 11:40:41 +0200

    main

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from conversation_general_mode import run_conversation_general_mode
 import os
 from datetime import datetime
 import nltk
+import json
+from retriever.embedding import Vectorstore
+
 
 # Set the NLTK data path to the specified directory
 data_dir = os.path.join(os.getcwd(), 'data')
 nltk.data.path.append(data_dir)
 
 # App title
-st.set_page_config(page_title="🧑‍⚕️💬 Meditron-3-70B 🩺")
+st.set_page_config(page_title="💬 Meditron-3-70B 🩺")
 
 # Function to save conversation to a txt file with Markdown formatting
 def save_conversation(conversation, mode, additional_patient_info=None):
@@ -60,11 +63,33 @@ if not os.path.exists("logs"):
 
 # Replicate Credentials
 with st.sidebar:
-    st.title("🧑‍⚕️💬 Meditron-3-70B 🩺")
+    st.title("💬 Meditron-3-70B 🩺")
 
     st.subheader('Mode Selection')
     selected_mode = st.radio('Choose a mode', ['ICRC-knowledge based chatbot', 'Conversation-patient', 'Conversation-general'], key='selected_mode')
 
+
+
+# Load the appropriate mode
+if selected_mode == 'ICRC-knowledge based chatbot':
+    if 'vector_store' not in st.session_state:
+        with open('retriever/books_embeddings_2.jsonl', 'r') as json_file:
+                documents_embeddings = json.load(json_file)
+        data = []
+        with open('retriever/books_split_final.jsonl', 'r') as json_file:
+            for line in json_file:
+                data.append(json.loads(line))
+        shape = 1024
+        print("heeeere")
+        st.session_state.vector_store = Vectorstore(document_embeddings=documents_embeddings, data=data, shape=shape)
+        print("there")
+    run_rag_mode(st.session_state.vector_store)
+elif selected_mode == 'Conversation-patient':
+    run_conversation_patient_mode()
+else:
+    run_conversation_general_mode()
+
+with st.sidebar:
     st.subheader('Save and clear chat history')
 
     # Add a button to save the conversation
@@ -75,11 +100,14 @@ with st.sidebar:
             save_conversation(st.session_state.conversation_messages, selected_mode, build_additional_information())
         elif selected_mode == 'Conversation-general' and "conversation_general_messages" in st.session_state:
             save_conversation(st.session_state.conversation_general_messages, selected_mode)
-
-# Load the appropriate mode
-if selected_mode == 'ICRC-knowledge based chatbot':
-    run_rag_mode()
-elif selected_mode == 'Conversation-patient':
-    run_conversation_patient_mode()
-else:
-    run_conversation_general_mode()
+    if st.sidebar.button("Clear Chat History"):
+        if selected_mode == 'ICRC-knowledge based chatbot' and "rag_messages" in st.session_state:
+            st.session_state.rag_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+        elif selected_mode == 'Conversation-patient' and "conversation_messages" in st.session_state:
+            st.session_state.conversation_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+            st.session_state['location'] = ''
+            st.session_state['age'] = None
+            st.session_state['Sex'] = 'Male'
+            st.session_state['travel_history'] = ''
+        elif selected_mode == 'Conversation-general' and "conversation_general_messages" in st.session_state:
+            st.session_state.conversation_general_messages = [{"role": "assistant", "content": "How may I assist you today?"}]

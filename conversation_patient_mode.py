2d2300057324b78291b90adba91cce76580245e4
cs210-bbernath~ <bastien.bernath@epfl.ch>
2024-08-16 09:31:00 +0200

    clear chat history

--- conversation_patient_mode.py
+++ conversation_patient_mode.py
@@ -68,14 +68,14 @@ def run_conversation_patient_mode():
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
-    def clear_chat_history():
-        st.session_state.conversation_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-        st.session_state['location'] = ''
-        st.session_state['age'] = None
-        st.session_state['Sex'] = 'Male'
-        st.session_state['travel_history'] = ''
+    # def clear_chat_history():
+    #     st.session_state.conversation_messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    #     st.session_state['location'] = ''
+    #     st.session_state['age'] = None
+    #     st.session_state['Sex'] = 'Male'
+    #     st.session_state['travel_history'] = ''
 
-    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    # st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
     def llm_generate_response(prompt_input):
         client = OpenAI(base_url="http://104.171.203.227:8000/v1", api_key="EMPTY")
